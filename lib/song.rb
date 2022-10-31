b91c33fd08464a1a946e2836a4491b3da46365e0
Evans Ochieng <evansochieng121@gmail.com>
2022-10-31 11:39:44 +0300

    practice mapping db records to ruby objects

--- lib/song.rb
+++ lib/song.rb
@@ -44,9 +44,42 @@ class Song
     self
   end
 
+  #method to instantiate and save 
   def self.create(name:, album:)
     song = Song.new(name: name, album: album)
     song.save
+  end
+
+  #convert db query results to ruby objects
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  #return all songs(instances) in the database and create ruby object for each row
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  #find a song by name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first #grab the first item from the array returned by #map
   end
 
 end
